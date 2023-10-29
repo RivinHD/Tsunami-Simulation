b2b505fb95a91232a8a32da2f326478df350cc55
RivinHD <58261670+RivinHD@users.noreply.github.com>
2023-10-29 14:43:13 +0100

    Improved test case documentation
    changed code-block formatting to cpp
    improved Visualization documentation
    + review at hmtl_issue7_unit_test_doc_improvement
    see issue #7

---
+++ docs_sphinx/review/hmtl_issue7_unit_test_doc_improvement/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['netupdates_0',['netupdates',['../classtsunami__lab_1_1solvers_1_1_f_wave.html#a6d19c9109a94599c4688e89d5cf0e6e4',1,'tsunami_lab::solvers::FWave::netUpdates()'],['../classtsunami__lab_1_1solvers_1_1_roe.html#afd6790dc03b4a684728ccdea0661c0eb',1,'tsunami_lab::solvers::Roe::netUpdates()']]]
+];
