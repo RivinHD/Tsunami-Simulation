0de8569e6d21065d1b87624d1ff6c7e31d4fc12f
Fabian Hofer <fabianhof02@gmail.com>
2023-11-06 16:34:08 +0100

    Added task 3.1.1
    Extended f-wave solver with support for bathymetry

--- include/solvers/FWave.h
+++ include/solvers/FWave.h
@@ -70,9 +70,23 @@ private:
                                          t_real & o_eigencoefficient1,
                                          t_real & o_eigencoefficient2);
 
+    /**
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_bL height of bathymetry of the left side.
+     * @param i_bR height of bathymetry of the right side.
+     * @param o_bathymetryEffect output: effect of the bathymetry
+     */
+    static void computeBathymetryEffects(t_real i_hL,
+                                         t_real i_hR,
+                                         t_real i_bL,
+                                         t_real i_bR,
+                                         t_real o_bathymetryEffect[2]);
+
 public:
     /**
-     * Computes the net-updates.
+     * Computes the net-updates without bathymetry.
      *
      * @param i_hL height of the left side.
      * @param i_hR height of the right side.
@@ -85,6 +99,27 @@ public:
                             t_real i_hR,
                             t_real i_huL,
                             t_real i_huR,
+                            t_real o_netUpdateL[2],
+                            t_real o_netUpdateR[2] );
+
+    /**
+     * Computes the net-updates with bathymetry.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param i_bL height of bathymetry of the left side.
+     * @param i_bR height of bathymetry of the right side.
+     * @param o_netUpdateL will be set to the net-updates for the left side; 0: height, 1: momentum.
+     * @param o_netUpdateR will be set to the net-updates for the right side; 0: height, 1: momentum.
+     **/
+    static void netUpdates( t_real i_hL,
+                            t_real i_hR,
+                            t_real i_huL,
+                            t_real i_huR,
+                            t_real i_bL,
+                            t_real i_bR,
                             t_real o_netUpdateL[2],
                             t_real o_netUpdateR[2] );
 };
